5e69a80ee75df6dbdb2cd067660e25d6a1fc7ce9
AD1024 <ccoderad@gmail.com>
2019-01-17 20:51:58 -0800

    [ add ] config exmaple

---
+++ src/frontend/config-example.js
@@ -0,0 +1,13 @@
+config = {
+    port: 8080,
+    db: {
+        name: 'uw_solar',
+        username: 'root',
+        password: 'root',
+        host: 'localhost',
+        dialect: 'mysql',
+    },
+    mode: 'dev'
+}
+
+module.exports = config;
