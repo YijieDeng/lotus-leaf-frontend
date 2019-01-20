9c724bf1d9bb5058ac0dd9a72c4bb964997b63ea
AD1024 <ccoderad@gmail.com>
2019-01-19 23:16:30 -0800

    [ fix ] date comparison error

--- src/frontend/routes/index.js
+++ src/frontend/routes/index.js
@@ -15,12 +15,12 @@ router.post('/query', async (ctx, next) => {
     var sample_rate = params.sample_rate
     const datetime_start = new Date(`${date_start} ${time_start}`)
     const datetime_end = new Date(`${date_end} ${time_end}`)
-    if (datetime_end < CURRENT_TIME || datetime_start < CURRENT_TIME) {
+    if (datetime_end > CURRENT_TIME || datetime_start > CURRENT_TIME) {
         ctx.body = {
             status: 'error',
             message: 'Error: Time start / Time end is the future'
         }
-    } else if (datetime_start < datetime_end) {
+    } else if (datetime_start > datetime_end) {
         ctx.body = {
             status: 'error',
             message: 'Error: Time end is before time start'
