09b2a5ae5e6115c508d9d0005ce2db9b9dca3f0d
AD1024 <dh63@uw.edu>
2019-05-15 11:04:06 -0700

    [ add ] setup script

---
+++ src/frontend/config-env.sh
@@ -0,0 +1,43 @@
+echo "UW Solar Data Frontend Server Configuation";
+
+sudo apt-get update;
+
+print_step () {
+    echo "Step "$1": $2";
+}
+
+install_package() {
+    print_step $1 "Install $2";
+    if ! foobar_loc="$(type -p "$3")" || [[ -z $foobar_loc ]]; then
+        sudo apt-get install $4;
+    else
+        echo "$3 has been installed";
+    fi
+}
+
+echo "------------------------------"
+echo "| Installing Requirements... |"
+echo "------------------------------"
+
+install_package 1 "NodeJS" "node" "nodejs"
+# install_package 2 "PM2" "pm2"
+install_package 2 "MySQL" "mysql" "mysql-server"
+
+echo "Starting MySQL Service...."
+sudo service mysql restart
+
+echo "----------------------"
+echo "| Configure Packages |"
+echo "----------------------"
+npm install
+sudo npm install pm2 -g
+
+echo "Requirement are installed"
+
+echo "This script does not operate database. Please initialize it"
+echo "manually by running:"
+echo "mysql -u <username> -p"
+echo "create database <name>"
+echo "and then modify your config.js according to the documentation."
+
+
